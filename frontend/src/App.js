e2b3b75796eb290521c1c855f6280cced2c20223
israrkhanuet <israrkhanuet@gmail.com>
2023-12-27 17:24:00 +0500

    mainsections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import PorductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='App'>
+      <div className='d-flex flex-column site-container'>
         <header className='App-header'>
-          <Link to='/'>amazon</Link>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>amazon</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path='/' element={<HomeScreen />} />
-            <Route path='/product/:slug' element={<PorductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} />
+              <Route path='/product/:slug' element={<PorductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>All right reserved </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
