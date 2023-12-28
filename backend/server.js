f3c91c4633eab31dc012743fa3b028634a6aa341
israrkhanuet <israrkhanuet@gmail.com>
2023-12-28 17:23:10 +0500

    CartSettingforsameobj

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,16 @@ app.get("/api/products/slug/:slug", (req, res) => {
   console.log("Product");
 });
 
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+  console.log("id");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
